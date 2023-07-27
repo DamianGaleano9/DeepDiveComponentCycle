79ba7cf580bbb49d47835c45ede5a35f88b5f2ae
Damian Galeano <dj.damian.b@hotmail.com>
2023-07-28 00:06:14 +0200

    Add State for Curren date

--- src/components/discussion.js
+++ src/components/discussion.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
 
 export default class Discussion extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            pageTitle: "Discussion",
+            currentTime: String(new Date())
+        }
+    }
     render() {
-        return <div>Discussion .......</div>
+        const { pageTitle, currentTime} = this.state;
+
+        return (
+            <div>
+                <h1>{pageTitle}</h1>
+                <h1>{currentTime}</h1>
+            </div>
+        )
 
     }
 }
